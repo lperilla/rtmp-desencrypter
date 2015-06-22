6a18f2fd611259ec24f9048949aae8930e879258
lperilla <luisperilla@gmail.com>
2015-06-22 18:55:33 +0200

    Configurando atajo de teclado para opciones del menu Se agrego menu de ayuda (No hace nada)

--- src/main/java/org/lperilla/rtmp/desencrypter/gui/MainMenuBar.java
+++ src/main/java/org/lperilla/rtmp/desencrypter/gui/MainMenuBar.java
@@ -1,12 +1,8 @@
 package org.lperilla.rtmp.desencrypter.gui;
 
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
-
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.KeyStroke;
 
 import org.lperilla.framework.core.action.ActionRouter;
 import org.lperilla.rtmp.desencrypter.actions.ActionNames;
@@ -35,6 +31,8 @@ public class MainMenuBar extends JMenuBar {
 	 */
 	private JMenuItem fileSave;
 
+	private JMenu helpMenu;
+
 	public MainMenuBar() {
 		this.init();
 	}
@@ -44,6 +42,7 @@ public class MainMenuBar extends JMenuBar {
 		this.getFileMenu().add(this.getFileSave());
 		this.getFileMenu().add(this.getFileExit());
 		this.add(this.getFileMenu());
+		this.add(this.getHelpMenu());
 	}
 
 	public JMenu getFileMenu() {
@@ -60,44 +59,37 @@ public class MainMenuBar extends JMenuBar {
 	public JMenuItem getFileExit() {
 		if (this.fileExit == null) {
 			this.fileExit = new JMenuItem("Exit");
-			//this.fileLoad.setAccelerator(KeyStrokes.EXIT);
 			this.fileExit.setActionCommand(ActionNames.EXIT);
 			this.fileExit.addActionListener(ActionRouter.getInstance());
 		}
 		return fileExit;
 	}
 
-	public void setFileExit(JMenuItem fileExit) {
-		this.fileExit = fileExit;
-	}
-
 	public JMenuItem getFileLoad() {
 		if (this.fileLoad == null) {
-			this.fileLoad = new JMenuItem("Abrir");
-//			KeyStroke OPEN = KeyStroke.getKeyStroke(KeyEvent.VK_O, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
-//			this.fileLoad.setAccelerator(OPEN);
+			this.fileLoad = new JMenuItem("Abrir", 'O');
+			this.fileLoad.setAccelerator(KeyStrokes.OPEN);
 			this.fileLoad.setActionCommand(ActionNames.OPEN);
 			this.fileLoad.addActionListener(ActionRouter.getInstance());
 		}
 		return fileLoad;
 	}
 
-	public void setFileLoad(JMenuItem fileLoad) {
-		this.fileLoad = fileLoad;
-	}
-
 	public JMenuItem getFileSave() {
 		if (this.fileSave == null) {
 			this.fileSave = new JMenuItem("Guardar", 'S');
-			this.fileLoad.setAccelerator(KeyStrokes.SAVE);
+			this.fileSave.setAccelerator(KeyStrokes.SAVE);
 			this.fileSave.setActionCommand(ActionNames.SAVE);
 			this.fileSave.addActionListener(ActionRouter.getInstance());
 		}
 		return fileSave;
 	}
 
-	public void setFileExport(JMenuItem fileExport) {
-		this.fileSave = fileExport;
+	public JMenu getHelpMenu() {
+		if (this.helpMenu == null){
+			this.helpMenu = new JMenu("Help");
+		}
+		return helpMenu;
 	}
 
 }
